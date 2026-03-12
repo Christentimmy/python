34d40159e9a51e0e8e4b846b68ee14425aace798
Christentimmy <timmychris09@gmail.com>
2026-03-12 21:59:37 +0300

    github analyzer

--- github_analyzer.py
+++ github_analyzer.py
@@ -1,40 +1,83 @@
 import asyncio
 import httpx
-from datetime import datetime
 
 base_url = "https://api.github.com/users"
 
 
-async def get_github_user(name: str):
+async def get_github_profile(name: str) -> dict:
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{base_url}/{name}")
-        if response.status_code != 200:
-            raise Exception(
-                f"Failed to fetch user data: {response.status_code}")
-        decoded = response.json()
-        created_at = datetime.fromisoformat(decoded["created_at"].rstrip("Z"))
-        formatted = datetime.strftime(created_at,"%d %b %Y")
-        return {
-            "name": decoded["name"],
-            "public_repos": decoded["public_repos"],
-            "followers": decoded["followers"],
-            "following": decoded["following"],
-            "created_at": formatted
-        }
+        try:
+            for _ in range(3):
+                response = await client.get(f"{base_url}/{name}", timeout=5.0)
+
+                if response.status_code == 429:
+                    await asyncio.sleep(2)
+                    continue
+
+                if response.status_code != 200:
+                    await asyncio.sleep(1)
+                    continue
+
+                decoded = response.json()
+
+                if not decoded:
+                    raise Exception("No data found")
+
+                return {
+                    "name": decoded["login"],
+                    "public_repos": decoded["public_repos"],
+                    "followers": decoded["followers"],
+                    "following": decoded["following"],
+                    "created_at": decoded["created_at"]
+                }
+
+        except httpx.RequestError as exc:
+            print(f"Connection error: {exc}. Retrying...")
+            await asyncio.sleep(1)
 
 
-res = asyncio.run(get_github_user("Christentimmy"))
-print(res)
-
-async def analyze_repos(name: str) -> dict:
+async def get_github_repo_stat(name: str) -> dict:
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{base_url}/{name}/repos")
-        if response.status_code != 200:
-            raise Exception("Failed to fetch")
-        decoded = response.json()
-        total_repos = len(decoded)
-        return {"total_repos": total_repos};
+        try:
+            for _ in range(3):
+                response = await client.get(f"{base_url}/{name}/repos", timeout=5.0)
+
+                if response.status_code == 429:
+                    await asyncio.sleep(2)
+                    continue
+
+                if response.status_code != 200:
+                    await asyncio.sleep(1)
+                    continue
+
+                decoded = response.json()
+
+                if not decoded:
+                    raise Exception("No data found")
+
+                total_repos = len(decoded)
+                most_starred_repo = max(
+                    decoded, key=lambda x: x["stargazers_count"])
+                return {
+                    "total_repos": total_repos,
+                    "most_starred_repo": most_starred_repo["name"]
+                }
+        except httpx.RequestError as exc:
+            print(f"Connection error: {exc}. Retrying...")
+            await asyncio.sleep(1)
 
 
-res = asyncio.run(analyze_repos("Christentimmy"))
+async def github_user_summary(name: str) -> dict:
+    try:
+        profile, repo_stat = await asyncio.gather(
+            get_github_profile(name),
+            get_github_repo_stat(name)
+        )
+        return {**profile, **repo_stat}
+    except Exception as exc:
+        print(f"Error fetching data for user {name}: {exc}")
+        return {}
+    
+
+res = asyncio.run(github_user_summary("octocat"))
 print(res)
